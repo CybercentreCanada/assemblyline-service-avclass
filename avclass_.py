900911cc915c869691bcc58a4abe743396876f24
cccs-bb <63248329+cccs-bb@users.noreply.github.com>
2021-03-03 19:44:49 +0000

    Refactored and added tests

--- avclass_.py
+++ avclass_.py
@@ -1,8 +1,8 @@
-import json
 from collections import namedtuple
-from pathlib import Path
-from typing import Optional, Dict, Any
 from itertools import groupby
+import json
+from pathlib import Path
+from typing import Optional, Dict, Any, List, Iterator
 
 from pkg_resources import resource_filename
 
@@ -20,13 +20,14 @@ TAX_PATH = DATA_PATH / 'avclass.taxonomy'
 AVClassTag = namedtuple('AVClassTag', ['name', 'path', 'category', 'rank'])
 AVClassTags = namedtuple('AVClassTags', ['tags', 'is_pup', 'family'])
 
+# Mapping of category abbreviation => (family name, heuristic ID (optional), result section tag type (optional))
 AVCLASS_CATEGORY = {
-    'FAM': ('family', 1),
-    'BEH': ('behavior', 2),
-    'CLASS': ('classification', 3),
-    'FILE': ('file', 4),
-    'GEN': ('generic', None),
-    'UNK': ('unknown', None),
+    'FAM': ('family', 1, None),
+    'BEH': ('behavior', 2, 'file.behavior'),
+    'CLASS': ('classification', 3, None),
+    'FILE': ('file', 4, None),
+    'GEN': ('generic', None, None),
+    'UNK': ('unknown', None, None),
 }
 AVCLASS_CATEGORY_ORDER = ['FAM', 'BEH', 'CLASS', 'FILE', 'GEN', 'UNK']
 
@@ -43,7 +44,20 @@ class AVclass(ServiceBase):
         self._av_labels = AvLabels(tag_file=TAG_PATH, exp_file=EXP_PATH,
                                    tax_file=TAX_PATH)
 
-    def _get_avclass_tags(self, sample_info):
+    def _get_avclass_tags(self, md5: str, sha1: str, sha256: str, labels: List[str]) -> Optional[AVClassTags]:
+        """
+        Gets AVclass tags from a list of AV labels.
+
+        :param md5: MD5 hash of file
+        :param sha1: SHA1 hash of file
+        :param sha256: SHA256 hash of file
+        :param labels: AV labels from "av.virus_name" result tags
+        :return: `AVClassTags` or `None` if no tags were extracted
+        """
+        sample_info = SampleInfo(md5, sha1, sha256,
+                                 [(f'av{i}', label) for i, label in enumerate(labels)], [])
+        self.log.debug(f'SampleInfo: {sample_info}')
+
         tokens = self._av_labels.get_sample_tags(sample_info)
         tags = self._av_labels.rank_tags(tokens)
         if not tags:
@@ -62,54 +76,86 @@ class AVclass(ServiceBase):
 
         return AVClassTags(avc_tags, self._av_labels.is_pup(tags, self._av_labels.taxonomy), family)
 
-    def execute(self, request: ServiceRequest) -> Optional[Dict[str, Any]]:
-        result = Result()
-        request.result = result
+    def _get_category_section(self, category: str, tags: Iterator[AVClassTag]) -> ResultSection:
+        """
+        Gets a `ResultSection` for a list of tags from a single category.
 
-        tags = request.task.tags.get('av.virus_name')
-        if not tags:
-            return
+        Result contains table with AVclass tag information in descending order by rank.
 
-        sample_info = SampleInfo(request.md5, request.sha1, request.sha256,
-                                 [(f'av{i}', label) for i, label in enumerate(tags)], [])
-        self.log.debug(f'SampleInfo: {sample_info}')
-        av_tags = self._get_avclass_tags(sample_info)
-        if av_tags is None:
-            return
+        :param category: Category of tags
+        :param tags: Tags belonging to category
+        :return: `ResultSection`
+        """
+        tags = sorted(tags, key=lambda t: t.rank, reverse=True)
 
-        body = {'is_pup': av_tags.is_pup}
-        tags = dict()
-        if av_tags.family is not None:
-            title = f'AVclass extracted malware family: {av_tags.family}'
-            body['family'] = av_tags.family
-            tags['attribution.family'] = [av_tags.family]
+        category_name, heur_id, tag_type = AVCLASS_CATEGORY[category]
+        tag_table = [{'name': tag.name,
+                      'category': category_name,
+                      'path': tag.path,
+                      'rank': tag.rank}
+                     for tag in tags]
+
+        section = ResultSection(
+            f'AVclass extracted {len(tags)} {category_name} tags',
+            body=json.dumps(tag_table), body_format=BODY_FORMAT.TABLE,
+            heuristic=Heuristic(heur_id) if heur_id is not None else None)
+        if tag_type is not None:
+            for tag in tags:
+                section.add_tag(tag_type, tag.name)
+
+        return section
+
+    def _get_category_sections(self, tags: List[AVClassTag]) -> Iterator[ResultSection]:
+        """
+        Returns a `ResultSection` for each category of AVclass tags.
+
+        :param tags: AVClass tags
+        :return: A `ResultSection` for each AVclass tag category
+        """
+        # Sort tags by category for grouping
+        tags = sorted(tags, key=lambda t: AVCLASS_CATEGORY_ORDER.index(t.category))
+
+        for category, category_tags in groupby(tags, key=lambda t: t.category):
+            yield self._get_category_section(category, category_tags)
+
+    def _get_result_section(self, family: Optional[str], is_pup: bool) -> Optional[ResultSection]:
+        """
+        Returns a `ResultSection` for AVclass tags.
+
+        :param family: Malware family name extracted by AVclass
+        :param is_pup: Whether AVclass detected PUP
+        :return: A `ResultSection`
+        """
+        body = {'is_pup': is_pup}
+        section_tags = dict()
+        if family is not None:
+            title = f'AVclass extracted malware family: {family}'
+            body['family'] = family
+            section_tags['attribution.family'] = [family]
         else:
             title = 'AVclass was unable to extract a malware family'
         section = ResultSection(title, json.dumps(body),
                                 body_format=BODY_FORMAT.KEY_VALUE,
-                                tags=tags)
+                                tags=section_tags)
+        return section
 
-        for category, tag_group in groupby(sorted(av_tags.tags,
-                                                  key=lambda t: AVCLASS_CATEGORY_ORDER.index(t.category)),
-                                           key=lambda t: t.category):
-            tag_group = sorted(tag_group, key=lambda t: t.rank, reverse=True)
-            tag_table = [{'name': tag.name,
-                          'category': AVCLASS_CATEGORY[category][0],
-                          'path': tag.path,
-                          'rank': tag.rank}
-                         for tag in tag_group]
-            heur_id = AVCLASS_CATEGORY[category][1]
+    def execute(self, request: ServiceRequest) -> Optional[Dict[str, Any]]:
+        result = Result()
+        request.result = result
 
-            tag_section = ResultSection(
-                f'AVclass extracted {AVCLASS_CATEGORY[category][0]} tags',
-                body=json.dumps(tag_table),
-                body_format=BODY_FORMAT.TABLE,
-                heuristic=Heuristic(heur_id) if heur_id is not None else None)
+        # Get AV labels from previous services
+        av_labels = request.task.tags.get('av.virus_name')
+        if not av_labels:
+            return
 
-            if category == 'BEH':
-                for tag in tag_group:
-                    tag_section.add_tag('file.behaviour', tag.name)
+        # Extract AVclass tags
+        av_tags = self._get_avclass_tags(request.md5, request.sha1, request.sha256, av_labels)
+        if av_tags is None:
+            return
 
+        # Build results
+        section = self._get_result_section(av_tags.family, av_tags.is_pup)
+        for tag_section in self._get_category_sections(av_tags.tags):
             section.add_subsection(tag_section)
 
         result.add_section(section)
